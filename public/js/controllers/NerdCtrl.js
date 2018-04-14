9cdc900848e6426dca26bd2619b3bb0860ae9e5c
Atul Rupnar <atulr@orowealth.com>
2018-04-14 21:30:07 +0530

    ifactor : added logout functionality

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -3,7 +3,7 @@ angular.module('NerdCtrl', []).controller('NerdController', function($scope) {
 	$scope.tagline = 'Nothing beats a pocket protector!';
 
 	$scope.logOut = function () {
-		$scope.session.clear();
+		//$scope.session.clear();
 		window.location.href = "/login";
 	}
 
