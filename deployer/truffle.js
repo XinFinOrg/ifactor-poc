17a12e5a10e16785f6cd1c8c102ed21f3fad4305
atul rupnar <atulrupnar@gmail.com>
2018-07-23 15:09:16 +0530

    ifactor : truffle changes for quorum deployment
     - truffle changes for quorum deployments

--- deployer/truffle.js
+++ deployer/truffle.js
@@ -1,9 +1,12 @@
 module.exports = {
-   networks: {
-   development: {
-   host: "localhost",
-   port: 8545,
-   network_id: "*" // Match any network id
-  }
- }
+  	networks: {
+	    development: {
+	      host: "localhost",
+	      from : "0x0638e1574728b6d862dd5d3a3e0942c3be47d996",
+	      port: 22001,
+	      network_id: "*", // Match any network id
+	      gas: "4200000",
+	      gasPrice : 0
+	    }
+	}
 };
