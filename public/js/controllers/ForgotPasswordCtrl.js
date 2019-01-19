5a853bb202c1036063173490e3898558c08be489
vrushalipanchal <vrushalipanchal1996@gmail.com>
2019-01-19 15:38:00 +0530

    Forgot Password Ctrl file added

---
+++ public/js/controllers/ForgotPasswordCtrl.js
@@ -0,0 +1,23 @@
+angular.module('ForgotPasswordCtrl', []).controller('ForgotPasswordController',['$scope', '$rootScope',
+ '$http', '$location', 'GetPost', 'Helper',  function($scope, $rootScope,  
+ 	$http, $location, GetPost, Helper) {
+
+		$scope.isLoggedIn = false;
+
+	$scope.forgotpassword = function() {
+		console.log('data in');
+		var data  = {input : $scope.input, url : '/forgot-password'};
+		console.log(data);
+		GetPost.post(data, function(err, resp) {
+			// console.log(err, resp);
+			return resp;
+	    });
+	}
+
+	$scope.login = function() {
+		window.location.href = "/login";
+		// $location.path('/signup');		
+	};
+ 
+
+}]);
