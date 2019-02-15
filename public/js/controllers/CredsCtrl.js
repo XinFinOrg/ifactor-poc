f61d1bd4c731ba8b8a1372a9330f0d40c3366a4e
vrushalipanchal <vrushalipanchal1996@gmail.com>
2019-02-15 13:30:50 +0530

    Changes in doc link and password

--- public/js/controllers/CredsCtrl.js
+++ public/js/controllers/CredsCtrl.js
@@ -1,6 +1,5 @@
 angular.module('CredsCtrl', []).controller('CredentialsController', ['$scope', '$window', function ($scope,$window) {
     $scope.redirectToNewTab = function () {
         $window.open('http://infactor.io/docs', '_blank');
-
     };
 }]);
