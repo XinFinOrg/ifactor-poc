3f84e746ea9bfefef4cce41135274dd67343d34f
Viral Pasad <viral712@gmail.com>
2019-02-19 12:16:09 +0530

    Checking buyTokens API flow for error

--- app/routes/index.js
+++ app/routes/index.js
@@ -591,7 +591,7 @@ router.post('/payInvoice', async (function(req, res) {
 
 router.post('/buyTokens', function(req, res) {
 	if (!req.isAuthenticated()) {
-		console.log('buyTokens API > req.isAuthenticated(): false');
+		console.log('buyTokens API > req.isAuthenticated(): false, req.user:', req.user);
 		return res.send({status : false});
 	} else { 
 	var address = req.body.address;
