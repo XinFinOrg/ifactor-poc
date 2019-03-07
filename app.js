6c32bfe09edc23fb0c63a105870d69dc5651fb8b
atul rupnar <atulrupnar@gmail.com>
2019-03-07 19:44:32 +0530

    quickbook : added quickbook routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./app/routes/index');
 var users = require('./app/routes/users');
+var quickbook = require('./app/routes/quickbook');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
+app.use('/quickbook', quickbook);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
