9675e7cdf68a0d4089caa0b630decc4e0ddc583d
Anil Chinchawale <anil24593@gmail.com>
2019-12-17 17:42:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var authLocal = require('./app/auth/local');
 var port = process.env.PORT || 6001; // set our port
 var url = 'mongodb://localhost:27017/ifactor';
 if (ENV == 'dev') {
-  url = 'mongodb://localhost:27017/ifactor';
+  url = 'mongodb://localhost:27017/sdrifactor';
 }
 
 
